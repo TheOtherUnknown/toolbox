ad98f602183088c27ea7ea99c071fa0de7d37278
Adam I <15039983+TheOtherUnknown@users.noreply.github.com>
2019-07-16 15:36:00 -0400

    I guess we need backups

--- scripts/pve2date.sh
+++ scripts/pve2date.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+set -eu # One fails, it all fails
+vzdump --all --mode snapshot --dumpdir /mnt # Mount backup beforehand
 apt update
 apt upgrade -y
 pve5to6
 systemctl stop pve-ha-lrm
 echo "Stopped LRM on this machine. Press a key once the LRM has been stopped on all machines in the cluster."
-read -n 1 -s
+read -n 1 -s # Stage 2
 systemctl stop pve-ha-crm
 echo "deb http://download.proxmox.com/debian/corosync-3/ stretch main" > /etc/apt/sources.list.d/corosync3.list
 apt update
 apt dist-upgrade --download-only -y
 apt dist-upgrade
 echo "Corosync is up to date. Press a key once all other machines in the cluster are up to date as well."
-read -n 1 -s
+read -n 1 -s # Stage 3
 systemctl start pve-ha-lrm
 systemctl start pve-ha-crm
 sed -i 's/stretch/buster/g' /etc/apt/sources.list
