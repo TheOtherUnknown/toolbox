b9af981126f6d5245fa7c1dd22b61a7e6415b3a6
Adam I <15039983+TheOtherUnknown@users.noreply.github.com>
2019-07-16 11:35:36 -0400

    Create pve2date.sh

---
+++ scripts/pve2date.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+apt update
+apt upgrade -y
+pve5to6
+systemctl stop pve-ha-lrm
+echo "Stopped LRM on this machine. Press a key once the LRM has been stopped on all machines in the cluster."
+read -n 1 -s
+systemctl stop pve-ha-crm
+echo "deb http://download.proxmox.com/debian/corosync-3/ stretch main" > /etc/apt/sources.list.d/corosync3.list
+apt update
+apt dist-upgrade --download-only -y
+apt dist-upgrade
+echo "Corosync is up to date. Press a key once all other machines in the cluster are up to date as well."
+read -n 1 -s
+systemctl start pve-ha-lrm
+systemctl start pve-ha-crm
+sed -i 's/stretch/buster/g' /etc/apt/sources.list
+for file in /etc/apt/sources.list.d/*; do sed -i 's/stretch/buster/g' $file; done
+apt update
+apt dist-upgrade -y
+rm /etc/apt/sources.list.d/corosync3.list
+echo "Update complete. Scheduling restart..."
+pvecm status
+shutdown -r 5min
