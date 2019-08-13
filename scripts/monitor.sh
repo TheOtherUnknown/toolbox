98e9788612cce411dd404c997684ff18eca44898
Adam Isaac <aci747@engineer.com>
2019-08-12 20:12:16 -0400

    Add monitor script

---
+++ scripts/monitor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+counter=0
+set -u # Exit with error if any vars are undefined
+while true; do
+    curl -sm 5 -I -H "User-Agent: CSG monitor/0.1" https://csg.ius.edu > /dev/null # Make an HTTP HEAD request, timeout after 5 secs
+    if [ $? -gt 0 ]; then # Did curl have a exit code > 0?
+        counter=$((counter + 1)) # Yes!
+        if [ $counter -eq 2 ]; then # Has curl failed for 60 secs?
+            message="csg.ius.edu has not responded to HTTP requests for 60 seconds."
+        curl -H "Content-Type: application/json" -X POST -d "{\"username\": \"HTTP monitor\", \"content\": \"$message\"}" $hookurl
+        fi
+        if [ $counter -eq 10 ]; then # Has curl failed for 5 mins?
+            message="csg.ius.edu has not responded to HTTP requests for 5 minutes.\nNo more alerts will be sent until the host comes back online."
+        curl -H "Content-Type: application/json" -X POST -d "{\"username\": \"HTTP monitor\", \"content\": \"$message\"}" $hookurl
+        fi
+    else # No!
+        counter=0
+    fi
+    sleep 30 # wait before the next check
+done
