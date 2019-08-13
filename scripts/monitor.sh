a953e0e0459740c5e2e7c1fe2167a0574f1d2582
Adam I <15039983+TheOtherUnknown@users.noreply.github.com>
2019-08-13 11:58:16 -0400

    Fail harder

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 counter=0
 set -u # Exit with error if any vars are undefined
+if [ -z $hookurl ]; then
+    >&2 echo 'Hook URL is not set!'
+    exit 1
+fi
 while true; do
     curl -sm 5 -I -H "User-Agent: CSG monitor/0.1" https://csg.ius.edu > /dev/null # Make an HTTP HEAD request, timeout after 5 secs
     if [ $? -gt 0 ]; then # Did curl have a exit code > 0?
