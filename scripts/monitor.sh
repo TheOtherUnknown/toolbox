bc1268224f2970adc4f3e0db504c32e55f77da7c
Adam I <15039983+TheOtherUnknown@users.noreply.github.com>
2019-08-14 15:23:06 -0400

    Globbing

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -11,11 +11,11 @@ while true; do
         counter=$((counter + 1)) # Yes!
         if [ $counter -eq 2 ]; then # Has curl failed for 60 secs?
             message="csg.ius.edu has not responded to HTTP requests for 60 seconds."
-        curl -H "Content-Type: application/json" -X POST -d "{\"username\": \"HTTP monitor\", \"content\": \"$message\"}" $hookurl
+        curl -H "Content-Type: application/json" -X POST -d "{\"username\": \"HTTP monitor\", \"content\": \"$message\"}" "$hookurl"
         fi
         if [ $counter -eq 10 ]; then # Has curl failed for 5 mins?
             message="csg.ius.edu has not responded to HTTP requests for 5 minutes.\nNo more alerts will be sent until the host comes back online."
-        curl -H "Content-Type: application/json" -X POST -d "{\"username\": \"HTTP monitor\", \"content\": \"$message\"}" $hookurl
+        curl -H "Content-Type: application/json" -X POST -d "{\"username\": \"HTTP monitor\", \"content\": \"$message\"}" "$hookurl"
         fi
     else # No!
         counter=0
